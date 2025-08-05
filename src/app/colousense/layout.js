6ddbb948cbff3b916baeecf4dc8256ec84123bbd
Mahi singh <aka.mahisingh@gmail.com>
2025-08-05 10:47:36 +0530

    Navbar added

--- src/app/colousense/layout.js
+++ src/app/colousense/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
-        <main>{children}</main>
+        {children}
       </body>
     </html>
   );
